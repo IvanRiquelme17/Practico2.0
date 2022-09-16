2743723417fedc4c56278eb31f418995a8f2de97
nicoriquelme20 <113646607+nicoriquelme20@users.noreply.github.com>
2022-09-16 16:35:05 -0300

    ciudad

--- src/practico2/pkg0/Ciudad.java
+++ src/practico2/pkg0/Ciudad.java
@@ -9,5 +9,39 @@ public class Ciudad {
     private int kmRuta;
 
     public Ciudad(String nombre, String ruta, int kmRuta) {
+        this.nombre = nombre;
+        this.ruta = ruta;
+        this.kmRuta = kmRuta;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public int getKmRuta() {
+        return kmRuta;
+    }
+
+    public void setKmRuta(int kmRuta) {
+        this.kmRuta = kmRuta;
+    }
+
+    @Override
+    public String toString() {
+        return "Ciudad{" + "nombre=" + nombre + ", ruta=" + ruta + ", kmRuta=" + kmRuta + '}';
+    }
+
+  
 }
