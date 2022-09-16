95e04f3e2273466bc53c74d32d833dd29338250d
Ivan <113646650+IvanRiquelme17@users.noreply.github.com>
2022-09-16 16:20:58 -0300

    Prueba2.0

--- src/practico2/pkg0/Auto.java
+++ src/practico2/pkg0/Auto.java
@@ -1,4 +1,11 @@
 package practico2.pkg0;
 
 public class Auto extends Vehiculo {
+
+    @Override
+    public int calcularCostoDeCombustible(int distancia) {
+        return super.calcularCostoDeCombustible(distancia); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
 }
--- src/practico2/pkg0/Camion.java
+++ src/practico2/pkg0/Camion.java
@@ -1,4 +1,10 @@
 package practico2.pkg0;
 
 public class Camion extends Vehiculo {
+
+    @Override
+    public int calcularCostoDeCombustible(int distancia) {
+        return super.calcularCostoDeCombustible(distancia); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
